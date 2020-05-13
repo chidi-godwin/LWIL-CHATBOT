bd48188a5ec440d65b7dfc8ee175dae826c0054d
gnc <chidieberen1998@gmail.com>
2020-05-14 00:37:51 +0100

    working version with ngrok

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, make_response, jsonify
+import json
+import os
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    date, time = req.get('queryResult').get('parameters').values()
+    date, time = formatTimeAndDate(time, date)
+    data = {
+        "fulfillmentText": "Your appointment has been set for {} at {}".\
+                        format(date, time)
+    }
+    #response = json.dumps(data, indent=4)
+    #post = make_response(response)
+    #post.headers['Content-Type'] = 'application/json'
+    #print(post)
+    return make_response(jsonify(data))
+
+def formatTimeAndDate(time, date):
+    date = date.split('T')[0]
+    hours, mins = time.split('T')[1].split('+')[0].split(':')[:2]
+    period = ' am' if int(hours) < 12 else ' pm'
+    hours = hours if int(hours) < 12 else str(int(hours)-12)
+    time = hours + ':' + mins + period
+    return (date, time)
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
