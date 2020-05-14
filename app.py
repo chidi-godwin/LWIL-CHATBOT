4b89e857acad1e1a99e77b01f7a027a58ae5f629
gnc <chidieberen1998@gmail.com>
2020-05-15 00:20:04 +0100

    resolved all webhook issues

--- app.py
+++ app.py
@@ -10,13 +10,44 @@ def index():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    date, time = req.get('queryResult').get('parameters').values()
+    action = req.setdefault('queryResult', {'acion': 'form'}).get('action')
+    print(action)
+    if action == "appointment":
+        return make_response(appointment(req))
+    elif action == "form":
+        return make_response(form_response())
+    
+    
+    
+    
+def appointment(data):
+    date, time = data.get('queryResult').get('parameters').values()
     date, time = formatTimeAndDate(time, date)
     data = {
         "fulfillmentText": "Your appointment has been set for {} at {}".\
                         format(date, time)
     }
-    return make_response(jsonify(data))
+    return jsonify(data)
+
+@app.route('/location', methods=['POST'])
+def location():
+    req = request.get_json(silent=True, force=True)
+    with open('data.json', 'w') as data:
+        json.dump(req, data)
+    return
+
+def form_response():
+    with open('data.json') as f:
+        form_data = json.loads(f.read())
+    date, time = form_data.get('Date').split('T')
+    form_data['Date'] = date
+    form_data['Time'] = time
+    data = {
+        "fulfillmentText": """{} your appointment has been set for {} at {}, you'll recieve a call on {} to remind you ahead of time""".\
+                        format(form_data['Name'], form_data['Date'], 
+                        form_data['Time'], form_data['Phone'])
+    }
+    return jsonify(data)
 
 def formatTimeAndDate(time, date):
     """
